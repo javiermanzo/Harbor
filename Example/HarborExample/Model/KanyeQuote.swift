2ff362261c4eb47710bd627d6a58651951a65062
Javier Manzo <javier.r.manzo@gmail.com>
2023-02-22 00:59:46 -0300

    Example added

---
+++ Example/HarborExample/Model/KanyeQuote.swift
@@ -0,0 +1,12 @@
+//
+//  KanyeQuote.swift
+//  HarborExample
+//
+//  Created by Javier Manzo on 21/02/2023.
+//
+
+import Foundation
+
+struct KanyeQuote: Codable {
+    let quote: String
+}
---
+++ Example/HarborExample/Services/KanyeQuoteGetService.swift
@@ -0,0 +1,33 @@
+//
+//  KanyeQuoteGetService.swift
+//  HarborExample
+//
+//  Created by Javier Manzo on 21/02/2023.
+//
+
+import Foundation
+import Harbor
+
+class KanyeQuoteGetService: HServiceProtocolWithResult {
+    typealias T = KanyeQuote
+
+    var url: String = "https://api.kanye.rest/"
+
+    var httpMethod: HHttpMethod = .get
+
+    var headers: [String : String]?
+
+    var queryParameters: [String : String]? = nil
+
+    var pathParameters: [String : String]? = nil
+
+    var body: [String : Any]? = nil
+
+    var needAuth: Bool = false
+
+    var timeout: TimeInterval = 5
+
+    init() {
+
+    }
+}
